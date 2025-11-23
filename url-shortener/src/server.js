d73441170cb83fc1cca5470dee8cb97050fdc322
DESKTOP-LD0F2LO\franc <francisivimal2002@gmail.com>
2025-11-23 20:04:22 +0530

    005 - CORS updation for deplyment

--- url-shortener/src/server.js
+++ url-shortener/src/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config({ path: "../.env"});
+dotenv.config({ path: "../.env" });
 
 import urlRoutes from "./routes/urlRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://url-shortener-frontend-ewoh.onrender.com",
+  methods: ["GET", "POST", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use("/", urlRoutes);
 
